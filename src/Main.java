b59b84232d1241fdb2a0027106e9d66460a33929
opilek <Jasopila@o2.pl>
2025-02-26 19:51:43 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+
+        Point p1=new Point();
+        p1.x=5.0;
+        p1.y=6.0;
+
+
+        System.out.println(p1);
+       System.out.println(p1.toSvg());
+
+        p1.translate(-6,5);
+        System.out.println(p1);
+
+        Point p2=p1.translated(-6,5);
+        System.out.println(p2);
+
+       Segment s=new Segment();
+       s.a=new Point();
+       s.a.x=1.0;
+       s.a.y=5.0;
+
+       s.b=new Point();
+       s.b.x=5.0;
+       s.b.y=9.0;
+
+        System.out.println(s.length());
+
+        // Inicjalizowanie tablicy segmentów
+        Segment[] arr = new Segment[2];
+
+        // Inicjalizowanie pierwszego segmentu
+        arr[0] = new Segment();
+        arr[0].a.x = 1.0;
+        arr[0].a.y = 2.0;
+        arr[0].b.x = 4.0;
+        arr[0].b.y = 6.0;
+
+        // Inicjalizowanie drugiego segmentu
+        arr[1] = new Segment();
+        arr[1].a.x = 0.0;
+        arr[1].a.y = 0.0;
+        arr[1].b.x = 7.5;
+        arr[1].b.y = 5.0;
+
+        // Wywołanie metody maxSegment() z klasy Segment
+        Segment result = Segment.maxSegment(arr);
+
+        // Wyświetlanie wyniku
+        if (result != null) {
+            System.out.println("Najdłuższy segment ma długość: " + result.length());
+        } else {
+            System.out.println("Tablica segmentów jest pusta.");
+        }
+
+
+
+
+
+
+
     }
 }
---
+++ src/Segment.java
@@ -0,0 +1,42 @@
+import static java.lang.Math.pow;
+public class Segment
+{
+
+    public Point a,b;
+
+
+    public double length()
+    {
+
+        return Math.sqrt(pow(b.x-a.x,2)+pow(b.y-a.y,2));
+    }
+
+
+    public static Segment maxSegment(Segment[] arr)
+    {
+        if (arr.length == 0)
+        {
+            return null;  // Zwrócenie null, jeśli tablica jest pusta
+        }
+
+        Segment max = arr[0];  // Zakłada, że pierwszy segment jest najdłuższy
+
+        for (int i = 1; i < arr.length; i++)
+        {  // Rozpoczynamy od drugiego segmentu
+            if (arr[i].length() > max.length())
+            {  // Porównujemy długość segmentu
+                max = arr[i];  // Jeśli segment jest dłuższy, przypisujemy go jako nowy "max"
+            }
+        }
+        return max;  // Zwracamy najdłuższy segment po zakończeniu pętli
+    }
+
+}
+
+
+
+
+
+
+
+
